9202c312795fe51b5c9a9ded623abca1b4790a49
Ondřej Kadlec <36775456+447937@users.noreply.github.com>
2019-12-07 17:31:36 +0100

    Update pgrep.sh
    mírné vylepšení kódu

--- pgrep.sh
+++ pgrep.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-alive=1
 clear
 
 echo "               ========= Fulltextový PDF vyhledávač ========="
 echo -n "> Adresář: "
 read kde
 
-while [ $alive -eq 1 ]
+while [ true ]
 do
-	echo -n "\n\n> Hledaný text: "
-	read co
-	pdfgrep -HiR "$co" $kde
-	echo "Hledal se text: $co"
+        echo "- Skript lze ukončit signálem přerušení Ctrl-C."
+        echo -n "\n\n> Hledat text: "
+        read co
+        pdfgrep -HiR "$co" $kde
+        echo "> Hledal se text: $co"
 done
-
-
